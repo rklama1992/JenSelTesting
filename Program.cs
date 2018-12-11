30c08469bc7faf7973e962b904e894898dad6964
RLAMA <rklama19921992@gmail.com>
2018-12-11 13:42:24 -0600

    another commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -24,6 +25,19 @@ namespace JenkinTestingProg
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
             driver.Navigate().GoToUrl("https://www.google.com/");
+
+        }
+        [Test]
+        public void TestTrue()
+        {
+            Assert.AreEqual(9, 9);
+
+        }
+
+       // [Test]
+        public void TestFalse()
+        {
+            Assert.AreEqual(9, 19);
 
         }
     }
